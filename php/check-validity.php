ce9051c5e6620ffa307b5681a8ebb8936e8bd3a4
somjdev <110367081+somjdev@users.noreply.github.com>
2024-09-12 14:01:38 +1200

    Update check-validity.php

--- php/check-validity.php
+++ php/check-validity.php
@@ -3,8 +3,8 @@
 session_start();
 $mysqli = require "../php/login-database.php";
 
-$user_id = $_SESSION['user_id'];
-$item_id = $_GET['id'];
+$userID = $_SESSION['user_id'];
+$itemID = $_GET['id'];
 
 // SQL query which returns information if the owner id matches current user id and the supplied id is valid
 $sql = "SELECT
@@ -15,7 +15,7 @@ $sql = "SELECT
 FROM
     `order_data`
 WHERE
-    `id` = '$_GET[id]' AND `owner_id` = $user_id";
+    `id` = '$_GET[id]' AND `owner_id` = $userID";
 
 $result = $mysqli->execute_query($sql);
 $data = $result->fetch_assoc();
@@ -23,7 +23,7 @@ $data = $result->fetch_assoc();
 // if the result is valid send get request to userpage containing retrieved information if not return user with no information and prompt an error message
 if ($result) {
     // Link contains GET data surrounding item information
-    header("Location:../html/userpage.php?edit&id=$item_id&title=$data[title]&type=$data[arrival_type]&date=$data[arrival_date]&period=$data[arrival_period]");
+    header("Location:../html/userpage.php?edit&id=$itemID&title=$data[title]&type=$data[arrivalType]&date=$data[arrivalDate]&period=$data[arrivalPeriod]");
 } else {
     header("Location: ../html/userpage.php");
 }
