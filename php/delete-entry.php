105823c02a0c28a66c6e23a5e7c0c05979e1d492
somjdev <110367081+somjdev@users.noreply.github.com>
2024-09-12 14:07:24 +1200

    Update delete-entry.php

--- php/delete-entry.php
+++ php/delete-entry.php
@@ -3,11 +3,11 @@
 session_start();
 $mysqli = require "../php/login-database.php";
 
-$owner_id = $_SESSION['user_id'];
-$item_id = $_GET['id'];
+$ownerID = $_SESSION['userID'];
+$itemID = $_GET['id'];
 
 // SQL to delete an entry from the order data table if the item id is valid and the owner id is the same as the current users id
-$sql = "DELETE FROM order_data WHERE id = '$item_id' AND owner_id = '$owner_id'";
+$sql = "DELETE FROM order_data WHERE id = '$itemID' AND owner_id = '$ownerID'";
 
 // if the query is successful delete the item and return the user if not return the user and show an error message
 if ($mysqli->query($sql) === TRUE) {
