4920282dcd258c35a23ab6040cd18ee6ad190a43
somjdev <110367081+somjdev@users.noreply.github.com>
2024-09-12 14:00:25 +1200

    Update add-entry.php

--- php/add-entry.php
+++ php/add-entry.php
@@ -3,15 +3,15 @@ session_start();
 $mysqli = require "../php/login-database.php";
 
 // defines all variables for ease of use
-$owner_id = $_SESSION["user_id"];
+$ownerID = $_SESSION["user_id"];
 $title = $_POST["title"];
-$arrival_type = $_POST["type"];
-$arrival_date = $_POST["arrival"];
-$arrival_period = $_POST["period"];
+$arrivalType = $_POST["type"];
+$arrivalDate = $_POST["arrival"];
+$arrivalPeriod = $_POST["period"];
 
 // Prepared SQL statement
 $sql = "INSERT INTO `order_data` (`id`, `owner_id`, `title`, `arrival_type`, `arrival_date`, `arrival_period`) 
-VALUES (NULL, '$owner_id', '$title', '$arrival_type', '$arrival_date', '$arrival_period')";
+VALUES (NULL, '$ownerID', '$title', '$arrivalType', '$arrivalDate', '$arrivalPeriod')";
 
 // executes SQL query if it is sucessful return user back if not display error message
 if ($mysqli->query($sql) === TRUE) {
